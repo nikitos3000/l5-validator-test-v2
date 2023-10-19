f81a85aa844cea482355f8f1a8244303e251f2ef
nikitos3000 <119944426+nikitos3000@users.noreply.github.com>
2023-10-19 14:33:39 +0300

    Update Validator.js

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,26 @@
+import ValidateArr from "./ArraySchema.js";
+import ValidateString from "./StringSchema.js";
+class Validator{
+    string(){
+       return new ValidateString
+    }
+    array(){
+        return new ValidateArr
+    }
+}
+const v = new Validator();
 
+const schema1 = v.string();
+schema1.isValid('hexlet'); // true;
+
+
+
+const schema2 = v.string().length(5)
+console.log(schema2.isValid('hexlet')) // false;
+console.log(schema2.isValid(' hello?')) // false;
+console.log(schema2.isValid('Hieee')) // true;
+console.log(schema2.isValid('!Hide')) // false;
+
+
+
+export default Validator
